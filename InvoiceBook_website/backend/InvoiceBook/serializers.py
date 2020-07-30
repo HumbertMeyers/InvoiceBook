4ef3cf9d01809a690aea35d53f00af53512d936e
Humbert Meyers <humbertmeyers@gmail.com>
2020-07-30 13:30:40 +0200

    suite création api REST

--- InvoiceBook_website/backend/InvoiceBook/serializers.py
+++ InvoiceBook_website/backend/InvoiceBook/serializers.py
@@ -1,18 +1,64 @@
 from rest_framework import serializers
 from .models import *
 
+##########################################
+#     Serializer concernant les users    #
+##########################################
 
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ('id_User', 'nom', 'email', 'password')
-		
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ('id_User', 'nom', 'email')
+
+
+class UserLoginGetTokenSerializer(serializers.ModelSerializer):
+	token = serializers.SerializerMethodField()
+	
+	def getToken(self, obj):
+		return obj.token
+	
+	class Meta:
+		model = User
+		fields = ('id_User', 'email', 'password', 'token')
+
+
+class NomSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ('nom',)
+
+
+##########################################
+# Serializer concernant les fournisseurs #
+##########################################
+
 class FournisseurSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Fournisseur
 		fields = ('id_Fournisseur', 'id_User', 'nom', 'numeroTVA')
-		
+
+
+##########################################
+#    Serializer concernant les clients   #
+##########################################
+
 class ClientSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Client
-		fields = ('id_Client', 'id_User', 'nom', 'numeroTVA')async
+		fields = ('id_Client', 'id_User', 'nom', 'numeroTVA')
+
+
+##########################################
+#   Serializer concernant les factures   #
+##########################################
+
+class FactureSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Facture
+		fields = ('id_Facture', 'dateFacturation', 'montant', 'nomDeFichier', 'id_User', 'id_Fournisseur', 'id_Client')
